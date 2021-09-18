6d1825c1a2beb76f9b33be3baf6dd465ccfbe00b
sromero50 <seba98asd@hotmail.com>
2021-09-18 08:36:08 +0000

    first upload

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [toDo, setToDo] = useState("");
+	const [list, setList] = useState([]);
+
+	const handleRemove = id => {
+		const newList = list.filter(item => item.id !== id);
+		setList(newList);
+	};
+	const handleSubmit = e => {
+		e.preventDefault();
+		setToDo("");
+	};
+
+	const handleOnChange = e => {
+		setToDo(e.target.value);
+	};
+
+	const handlePressKey = e => {
+		if (e.key === "Enter") {
+			setList([...list, { id: list.length + 1, content: toDo }]);
+		}
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="mt-5 container box form-group">
+			<h1 className="display-4">To do list</h1>
+			<form action="" onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="To Do"
+					className="form-control"
+					value={toDo}
+					onChange={handleOnChange}
+					onKeyPress={handlePressKey}
+				/>
+			</form>
+			<ul className="list-group list-group-flush">
+				{list.map(item => {
+					return (
+						<li key={item.id} className="list-group-item ">
+							<>{item.content}</>
+							<span onClick={() => handleRemove(item.id)}>
+								<i className="fas fa-times"></i>
+							</span>
+						</li>
+					);
+				})}
+			</ul>
+			<footer>{list.length} item left</footer>
 		</div>
 	);
 };
